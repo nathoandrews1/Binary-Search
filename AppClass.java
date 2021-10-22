1e5663ebe0ebd299e8dfc8e9386de6fa09d1c892
Nathan Andrews <nathoandrews@gmail.com>
2021-10-22 19:11:04 +0100

    App class where the functions are ran and printed results to the screen, in this case O(1) for binary.

---
+++ AppClass.java
@@ -0,0 +1,17 @@
+/*   */ import projectca2.MyArrayList;
+/*   */ 
+/*   */ public class AppClass {
+/*   */   public static void main(String[] args) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public static void fillArrayList(MyArrayList object, int arraySize) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */ }
+
+
+/* Location:              C:\Users\Nathan\Desktop\____GitHub_CollegeWork\_JavaDecompiler\!\AppClass.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
