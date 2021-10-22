7674720a138d2288f18c1d351d58e279948b6cde
Nathan Andrews <nathoandrews@gmail.com>
2021-10-22 19:16:30 +0100

    ArrayList class that extends ArrayList so we can populate and run the methods very easily, this was a project and heavily commented code to explain understanding. Looking over it now I can comment this better.

---
+++ MyArrayList.java
@@ -0,0 +1,39 @@
+/*   */ import java.util.ArrayList;
+/*   */ 
+/*   */ public class MyArrayList<ArrayType> extends ArrayList<ArrayType> {
+/*   */   private int searchAmount;
+/*   */   
+/*   */   int binarySearch(ArrayType item, int start, int end) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public int linearSearch(ArrayType item) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public int getSearchAmount() {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public void runLinearSearch(ArrayType item) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public void runBinarySearch(ArrayType item, int start, int end) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public String runBinarySearchGUI(ArrayType item, int start, int end) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */   
+/*   */   public String runLinearSearchGUI(ArrayType item) {
+/* 1 */     throw new RuntimeException("");
+/*   */   }
+/*   */ }
+
+
+/* Location:              C:\Users\Nathan\Desktop\____GitHub_CollegeWork\_JavaDecompiler\!\MyArrayList.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
